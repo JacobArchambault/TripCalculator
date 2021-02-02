f9c54b9707ee2457e7cf6f9a852b00bfc8deb51b
Jacob Archambault <jacob.archambault@waystar.com>
2021-02-02 15:24:26 -0500

    Add calculateMPG function and use it to display city data

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function displayData() {
+    appendTHs("table-head", ["Destination", "Miles", "Gallons", "MPG"]);
     const chicago = { destination: "Chicago", miles: 329, gallons: 10.6 };
     const stLouis = { destination: "St. Louis", miles: 284, gallons: 10.5 };
     const indianapolis = { destination: "Indianapolis", miles: 122, gallons: 3.7 };
@@ -6,8 +7,7 @@ function displayData() {
     const cincinnati = { destination: "Cincinnati", miles: 132, gallons: 3.9 };
 
     const cities = [chicago, stLouis, indianapolis, nashville, cincinnati];
-    appendTHs("table-head", ["Destination", "Miles", "Gallons", "MPG"]);
-    cities.forEach(x => appendRow("table-body", [x.destination, x.miles, x.gallons], "td"));
+    cities.forEach(x => appendTDs("table-body", [x.destination, x.miles, x.gallons, calculateMPG(x.miles, x.gallons).toFixed(1)]));
 }
 
 function append(parentElement, childElementText, childElementType) {
@@ -15,6 +15,7 @@ function append(parentElement, childElementText, childElementType) {
     data.appendChild(document.createTextNode(childElementText));
     parentElement.appendChild(data);
 }
+
 function appendRow(parentId, dataArray, childElementType) {
     const myRow = document.createElement("tr");
     document.getElementById(parentId).appendChild(myRow);
@@ -26,10 +27,15 @@ function appendRow(parentId, dataArray, childElementType) {
 function appendTH(parent, headerText) {
     append(parent, headerText, "th");
 }
+
 function appendTHs(parentId, headerTextArray) {
-    const myRow = document.createElement("tr");
-    document.getElementById(parentId).appendChild(myRow);
-    headerTextArray.forEach(element => {
-        appendTH(myRow, element);
-    });
+    appendRow(parentId, headerTextArray, "th");
+}
+
+function appendTDs(parentId, dataTextArray){
+    appendRow(parentId, dataTextArray, "td");
+}
+
+function calculateMPG(miles, gallons){
+    return miles/gallons;
 }
